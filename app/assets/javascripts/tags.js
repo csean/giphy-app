9545b5439b353f426fb8ac9851f09d9a1f4da75b
csean <sean@seanstowers.com>
2019-06-12 00:41:46 -0500

    fix js

--- app/assets/javascripts/tags.js
+++ app/assets/javascripts/tags.js
@@ -1,12 +1,2 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-$(document).ready(function() {
-  $('#tag').on('click', '.search-navigation', function(e) {
-    tag = $('#tag').data('tag');
-    e.preventDefault();
-    $.ajax({
-      url: '/tags/' + ,
-      data: { query: $('#query').val(), page: $(this).data('page') }
-    });
-  });
-})
