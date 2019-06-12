362ff4d556a89cd2ded1bd6f65d6b7c1863e6cbe
csean <sean@seanstowers.com>
2019-06-12 01:13:01 -0500

    fix devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[username name])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:username])
   end
 
   def after_sign_in_path(_resource_or_scope)
